49b26d9e36fc17eea3fca5553255c02ae3855086
SergeySudaraenko <159451661+SergeySudaraenko@users.noreply.github.com>
2024-04-08 20:31:08 +0200

    +

---
+++ goit-pycore-hw-08.py
@@ -0,0 +1,26 @@
+import pickle
+
+class AddressBook:
+    pass  
+
+def save_data(book, filename="addressbook.pkl"):
+    with open(filename, "wb") as f:
+        pickle.dump(book, f)
+
+def load_data(filename="addressbook.pkl"):
+    try:
+        with open(filename, "rb") as f:
+            return pickle.load(f)
+    except FileNotFoundError:
+        return AddressBook()
+
+def main():
+    
+    book = load_data()
+
+    save_data(book)
+
+if __name__ == "__main__":
+    main()
+
+
